405d62470d5d07ba225f3121c40766bc3cd1129e
Nishtha Yadav <95894895+Nishthayadava@users.noreply.github.com>
2024-12-01 01:14:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -56,6 +56,8 @@ app.post('/api/login', async (req, res) => {
 
             // Compare the hashed password with the stored one
             const passwordMatch = bcrypt.compareSync(trimmedPassword, user.password);
+                        console.log("passwordMatch",passwordMatch)
+
             if (passwordMatch) {
                 // If the password matches, generate a JWT token and return user data
                 const token = generateToken(user);
