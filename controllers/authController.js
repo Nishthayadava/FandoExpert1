06c74c8bb01396d570be4ba7affaa6d24fedaf9f
Nishtha Yadav <95894895+Nishthayadava@users.noreply.github.com>
2024-12-05 22:29:53 +0530

    Create authController.js

---
+++ controllers/authController.js
@@ -0,0 +1,25 @@
+const pool = require('../models/db');
+const jwt = require('jsonwebtoken');
+
+const generateToken = (user) => {
+    return jwt.sign({ id: user.id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
+};
+
+const login = async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const userQuery = await pool.query('SELECT * FROM users WHERE name = $1 AND password=$2', [username, password]);
+        if (userQuery.rows.length > 0) {
+            const user = userQuery.rows[0];
+            const token = generateToken(user);
+            return res.json({ token, role: user.role, userId: user.id });
+        } else {
+            return res.status(404).json({ message: 'User not found' });
+        }
+    } catch (error) {
+        console.error('Error during login:', error);
+        return res.status(500).json({ message: 'Internal server error' });
+    }
+};
+
+module.exports = { login };
