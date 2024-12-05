1584dd53e40deffc6667cc03375a686e7f081860
Nishtha Yadav <95894895+Nishthayadava@users.noreply.github.com>
2024-12-05 22:36:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const dotenv = require('./config/dotenv');
-const authRoutes = require('./routes/authRoutes');
-const attendanceRoutes = require('./routes/attendanceRoutes');
-const userRoutes = require('./routes/userRoutes');
-const leadRoutes = require('./routes/leadRoutes');
-const adminRoutes = require('./routes/adminRoutes');
+const routes = require('./routes'); // Import the consolidated routes
 
 // Initialize app
 const app = express();
@@ -16,12 +12,8 @@ dotenv.config();  // Load environment variables
 app.use(cors());
 app.use(bodyParser.json());
 
-// Routes
-app.use('/api', authRoutes);
-app.use('/api/attendance', attendanceRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/leads', leadRoutes);
-app.use('/api/admin', adminRoutes);
+// Use the routes from the consolidated routes file
+app.use(routes);
 
 // Server setup
 const PORT = process.env.PORT || 5000;
