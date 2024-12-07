0ea83cb1a88bd232d9db0e1a689a8c3c8fced963
Nishtha Yadav <95894895+Nishthayadava@users.noreply.github.com>
2024-12-08 00:40:17 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,15 +1,9 @@
 const pool = require('../models/db');
 const jwt = require('jsonwebtoken');
+const { aauthenticateToken, refreshToken,generateToken,generateAccessToken} = require('../middlewares/authMiddleware');
 
-const generateToken = (user) => {
-    const { id, role } = user;
-    return jwt.sign({ id: id.trim(), role: role.trim() }, process.env.JWT_SECRET, { expiresIn: '1h' });
-};
 
-const generateRefreshToken = (user) => {
-    const { id, role } = user;
-    return jwt.sign({ id: id.trim(), role: role.trim() }, process.env.JWT_REFRESH_SECRET, { expiresIn: '7d' });
-};
+
 
 const login = async (req, res) => {
     let { username, password } = req.body;
@@ -19,9 +13,17 @@ const login = async (req, res) => {
         const userQuery = await pool.query('SELECT * FROM users WHERE name = $1 AND password=$2', [username, password]);
         if (userQuery.rows.length > 0) {
             const user = userQuery.rows[0];
-            const token = generateToken(user);
-            return res.json({ token, role: user.role, userId: user.id });
-        } else {
+
+            const token = generateToken(user);            
+            const refreshToken = generateRefreshToken(user);
+            
+
+            return res.json({
+                token: token,  // Access token (1 hour)
+                refreshToken: refreshToken,  // Refresh token (7 days)
+                role: user.role,
+                userId: user.id
+            });        } else {
             return res.status(404).json({ message: 'User not found' });
         }
     } catch (error) {
