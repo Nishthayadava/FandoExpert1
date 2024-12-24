cb5070184bc99c34817bf75474f65c5ceac4901b
Nishtha Yadav <95894895+Nishthayadava@users.noreply.github.com>
2024-12-25 01:48:46 +0530

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -34,7 +34,7 @@ router.put('/api/updatelead/:id',  updateLead); // Update lead information
 router.get('/api/getleads', getLeads);
 // Assign agent to leads
 router.post('/api/assignagent',authenticateToken, assignAgent);
-router.get('/my-leads',authenticateToken, getMyLeads);
+router.get('/my-leads', getMyLeads);
 router.patch('/update-lead-status', authenticateToken, updateLeadStatus);
 
 
